84b1fa636b644caa32364ed9d6ceef1206c68587
kylie-amade <160512873+kylie-amade@users.noreply.github.com>
2024-03-01 00:34:47 +0300

    added changes

--- new.js
+++ new.js
@@ -16,7 +16,31 @@ function search(event) {
 
   axios.get(apiUrl).then(displayTemperature);
 }
-
+function formatDate(date){
+    let minutes = date.getMinutes();
+    let hours = date.getHours();
+    let day = date.getDay();
+    if (minutes<10){
+        minutes = `0${minutes}`;
+    }
+    if (hours<10){
+        hours = `0${hours}`;
+    }
+    let days = [
+        "Sunday",
+        "Monday",
+        "Tuesday",
+        "Wednesday",
+        "Thursday",
+        "Friday",
+        "Saturday",
+    ];
+    let formattedDay = days[day];
+    return`${formattedDay} ${hours}:${minutes}`;
+}
+let currentDateElement = document.querySelector("#current-date");
+let currentDate = new Date();
+currentDateElement.innerHTML = formatDate(currentDate);
 
 let searchForm = document.querySelector("#search-form");
 searchForm.addEventListener("submit", search);
